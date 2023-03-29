ace0a522fac39d0dfaf033df00447dc4d31d1968
Jameson Wang <jamesonw10@gmail.com>
2023-03-29 15:47:56 -0400

    adjust password length

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs')
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
@@ -16,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(500);
     throw new Error('Please fill in all required fields');
   }
-  if (password.length <= 6) {
+  if (password.length < 6) {
     throw new Error('Password gotta be 6 characters or more');
   }
 
@@ -59,7 +60,50 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // * LOGIN USER
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('test login run');
+  const { email, password } = req.body;
+
+  // Validate Request
+  if (!email || !password) {
+    res.status(401);
+    throw new Error('Incorrect email and/or password');
+  }
+
+  // Check if user exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('This email do not exist');
+  }
+
+  // If user exists, check if password is correct
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  // Generate token
+  const token = generateToken(user._id);
+
+  // Send HTTP-only cookie, if password matches
+  if (matchPassword) {
+    res.cookie('token', token, {
+      path: '/',
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400),
+      sameSite: 'none',
+      secure: true,
+    });
+  }
+
+  // Display after logging in
+  if (user && matchPassword) {
+    const { _id, name, email } = user
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      token,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid email and/or password')
+  }
 });
 
 module.exports = {
